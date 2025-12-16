8d39fe1c14ad454b090ffad8daa8675d08be7695
Alperen Aydın <aa.aydinalperen@gmail.com>
2025-12-16 23:01:21 +0300

    Add project files.

---
+++ ImageEditor/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace ImageEditor
+{
+    partial class FrmImageEditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmImageEditor));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblCropSize = new System.Windows.Forms.Label();
+            this.btnSaveNext = new System.Windows.Forms.Button();
+            this.btnSkip = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnFolder = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkGray;
+            this.panel1.Controls.Add(this.lblCropSize);
+            this.panel1.Controls.Add(this.btnSaveNext);
+            this.panel1.Controls.Add(this.btnSkip);
+            this.panel1.Controls.Add(this.lblStatus);
+            this.panel1.Controls.Add(this.btnFolder);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 587);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1264, 94);
+            this.panel1.TabIndex = 1;
+            // 
+            // lblCropSize
+            // 
+            this.lblCropSize.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblCropSize.Font = new System.Drawing.Font("Corbel", 15.75F);
+            this.lblCropSize.Location = new System.Drawing.Point(204, 16);
+            this.lblCropSize.Name = "lblCropSize";
+            this.lblCropSize.Size = new System.Drawing.Size(658, 30);
+            this.lblCropSize.TabIndex = 4;
+            this.lblCropSize.Text = "Yeni Boyut: -";
+            this.lblCropSize.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnSaveNext
+            // 
+            this.btnSaveNext.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSaveNext.BackColor = System.Drawing.Color.LightGreen;
+            this.btnSaveNext.Font = new System.Drawing.Font("Corbel", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSaveNext.Location = new System.Drawing.Point(1074, 20);
+            this.btnSaveNext.Name = "btnSaveNext";
+            this.btnSaveNext.Size = new System.Drawing.Size(152, 58);
+            this.btnSaveNext.TabIndex = 3;
+            this.btnSaveNext.Text = "Save";
+            this.btnSaveNext.UseVisualStyleBackColor = false;
+            this.btnSaveNext.Click += new System.EventHandler(this.btnSaveNext_Click);
+            // 
+            // btnSkip
+            // 
+            this.btnSkip.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSkip.BackColor = System.Drawing.Color.IndianRed;
+            this.btnSkip.Font = new System.Drawing.Font("Corbel", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSkip.Location = new System.Drawing.Point(887, 20);
+            this.btnSkip.Name = "btnSkip";
+            this.btnSkip.Size = new System.Drawing.Size(152, 58);
+            this.btnSkip.TabIndex = 2;
+            this.btnSkip.Text = "Skip";
+            this.btnSkip.UseVisualStyleBackColor = false;
+            this.btnSkip.Click += new System.EventHandler(this.btnSkip_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblStatus.Font = new System.Drawing.Font("Corbel", 15.75F);
+            this.lblStatus.Location = new System.Drawing.Point(199, 55);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(663, 30);
+            this.lblStatus.TabIndex = 1;
+            this.lblStatus.Text = "Image x/x";
+            this.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnFolder
+            // 
+            this.btnFolder.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.btnFolder.BackColor = System.Drawing.Color.LightBlue;
+            this.btnFolder.Font = new System.Drawing.Font("Corbel", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnFolder.Location = new System.Drawing.Point(36, 18);
+            this.btnFolder.Name = "btnFolder";
+            this.btnFolder.Size = new System.Drawing.Size(152, 58);
+            this.btnFolder.TabIndex = 0;
+            this.btnFolder.Text = "Select Folder";
+            this.btnFolder.UseVisualStyleBackColor = false;
+            this.btnFolder.Click += new System.EventHandler(this.btnFolder_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1264, 587);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // FrmImageEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FrmImageEditor";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Image Editor";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FrmImageEditor_FormClosing);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnFolder;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnSaveNext;
+        private System.Windows.Forms.Button btnSkip;
+        private System.Windows.Forms.Label lblCropSize;
+    }
+}
+
---
+++ ImageEditor/Form1.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageEditor
+{
+    public partial class FrmImageEditor : Form
+    {
+
+        private Bitmap originalBitmap;
+        private List<string> imagePaths = new List<string>();
+        private int currentIndex = -1;
+        private string outputFolder = "";
+
+        private bool isSelecting = false;
+        private Point startPoint;
+        private Rectangle selectionRect = Rectangle.Empty;
+
+        private DateTime lastUiUpdate = DateTime.MinValue;
+        private const int UiUpdateIntervalMs = 16; // ~60 FPS (33 yaparsan ~30 FPS)
+        private int lastCropW = -1;
+        private int lastCropH = -1;
+
+
+        public FrmImageEditor()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true;
+
+        }
+
+        private void btnFolder_Click(object sender, EventArgs e)
+        {
+
+            // using var yok -> klasik using bloğu
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog())
+            {
+                if (fbd.ShowDialog() != DialogResult.OK)
+                    return;
+
+                string folder = fbd.SelectedPath;
+
+                // Kaydedilecek klasör: seçilen klasör\Kirpilmis
+                outputFolder = Path.Combine(folder, "Kirpilmis");
+
+                if (!Directory.Exists(outputFolder))
+                    Directory.CreateDirectory(outputFolder);
+
+                HashSet<string> exts = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+                {
+                    ".jpg", ".jpeg", ".png", ".bmp"
+                };
+
+                imagePaths = Directory.GetFiles(folder)
+                    .Where(p => exts.Contains(Path.GetExtension(p)))
+                    .OrderBy(p => p)
+                    .ToList();
+
+
+                if (imagePaths.Count == 0)
+                {
+                    MessageBox.Show("Bu klasörde .jpg/.jpeg/.png/.bmp bulunamadı!");
+                    return;
+                }
+
+                currentIndex = -1;
+                LoadNextImage();
+            }
+        }
+
+        private void btnSkip_Click(object sender, EventArgs e)
+        {
+            LoadNextImage();
+        }
+
+        private void btnSaveNext_Click(object sender, EventArgs e)
+        {
+            if (originalBitmap == null)
+                return;
+
+            if (selectionRect.Width < 5 || selectionRect.Height < 5)
+            {
+                MessageBox.Show("Önce kırpma alanı seçmelisin.");
+                return;
+            }
+
+            Rectangle cropRect = TranslateSelectionToImageRect(pictureBox1, originalBitmap, selectionRect);
+            if (cropRect == Rectangle.Empty)
+            {
+                MessageBox.Show("Seçim geçersiz (görüntü alanının dışında olabilir).");
+                return;
+            }
+
+            Bitmap cropped = null;
+            try
+            {
+                cropped = originalBitmap.Clone(cropRect, originalBitmap.PixelFormat);
+
+                string srcPath = imagePaths[currentIndex];
+                string name = Path.GetFileNameWithoutExtension(srcPath);
+                string ext = Path.GetExtension(srcPath);
+
+                string outPath = Path.Combine(outputFolder, name + "_Kirpilmis" + ext);
+
+                cropped.Save(outPath);
+            }
+            finally
+            {
+                if (cropped != null) cropped.Dispose();
+            }
+
+            LoadNextImage();
+        }
+
+        private void LoadNextImage()
+        {
+            selectionRect = Rectangle.Empty;
+            isSelecting = false;
+
+            currentIndex++;
+
+            if (imagePaths == null || currentIndex >= imagePaths.Count)
+            {
+                pictureBox1.Image = null;
+
+                if (originalBitmap != null)
+                {
+                    originalBitmap.Dispose();
+                    originalBitmap = null;
+                }
+
+                lblStatus.Text = "Bitti.";
+                return;
+            }
+
+            if (originalBitmap != null)
+            {
+                originalBitmap.Dispose();
+                originalBitmap = null;
+            }
+
+            originalBitmap = new Bitmap(imagePaths[currentIndex]);
+            pictureBox1.Image = originalBitmap;
+
+            lblStatus.Text =
+                (currentIndex + 1).ToString() + "/" + imagePaths.Count.ToString()
+                + " - (" + originalBitmap.Width + " x " + originalBitmap.Height + ")"
+                + " - " + Path.GetFileName(imagePaths[currentIndex]);
+
+
+            pictureBox1.Invalidate();
+
+            lblCropSize.Text = "Yeni Boyut: -";
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if (selectionRect != Rectangle.Empty)
+            {
+                using (Pen pen = new Pen(Color.Lime, 2))
+                {
+                    e.Graphics.DrawRectangle(pen, selectionRect);
+                }
+            }
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (originalBitmap == null) return;
+
+            isSelecting = true;
+            startPoint = e.Location;
+            selectionRect = new Rectangle(e.Location, new Size(0, 0));
+            pictureBox1.Invalidate();
+
+            lastCropW = -1;
+            lastCropH = -1;
+            lastUiUpdate = DateTime.MinValue;
+
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!isSelecting) return;
+
+            int x = Math.Min(startPoint.X, e.X);
+            int y = Math.Min(startPoint.Y, e.Y);
+            int w = Math.Abs(startPoint.X - e.X);
+            int h = Math.Abs(startPoint.Y - e.Y);
+
+            selectionRect = new Rectangle(x, y, w, h);
+
+            // Çok sık Invalidate yapma (UI'yı boğar)
+            DateTime now = DateTime.Now;
+            if ((now - lastUiUpdate).TotalMilliseconds < UiUpdateIntervalMs)
+                return;
+
+            lastUiUpdate = now;
+
+            pictureBox1.Invalidate(); // 60 fps civarı çiz
+
+            if (originalBitmap != null && selectionRect.Width > 0 && selectionRect.Height > 0)
+            {
+                Rectangle cropRect = TranslateSelectionToImageRect(pictureBox1, originalBitmap, selectionRect);
+
+                if (cropRect != Rectangle.Empty)
+                {
+                    // Label'ı sadece gerçekten değiştiyse güncelle
+                    if (cropRect.Width != lastCropW || cropRect.Height != lastCropH)
+                    {
+                        lastCropW = cropRect.Width;
+                        lastCropH = cropRect.Height;
+                        lblCropSize.Text = "Yeni Boyut: " + lastCropW + " x " + lastCropH + " px";
+                    }
+                }
+                else
+                {
+                    if (lastCropW != -1 || lastCropH != -1)
+                    {
+                        lastCropW = -1;
+                        lastCropH = -1;
+                        lblCropSize.Text = "Yeni Boyut: -";
+                    }
+                }
+            }
+
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isSelecting = false;
+            pictureBox1.Invalidate();
+        }
+
+        private Rectangle TranslateSelectionToImageRect(PictureBox pb, Bitmap img, Rectangle sel)
+        {
+            Rectangle displayRect = GetImageDisplayRectangle(pb, img);
+
+            Rectangle intersect = Rectangle.Intersect(sel, displayRect);
+            if (intersect.Width <= 0 || intersect.Height <= 0)
+                return Rectangle.Empty;
+
+            float scaleX = (float)img.Width / (float)displayRect.Width;
+            float scaleY = (float)img.Height / (float)displayRect.Height;
+
+            int x = (int)((intersect.X - displayRect.X) * scaleX);
+            int y = (int)((intersect.Y - displayRect.Y) * scaleY);
+            int w = (int)(intersect.Width * scaleX);
+            int h = (int)(intersect.Height * scaleY);
+
+            if (x < 0) x = 0;
+            if (y < 0) y = 0;
+
+            if (x >= img.Width) x = img.Width - 1;
+            if (y >= img.Height) y = img.Height - 1;
+
+            if (w < 1) w = 1;
+            if (h < 1) h = 1;
+
+            if (x + w > img.Width) w = img.Width - x;
+            if (y + h > img.Height) h = img.Height - y;
+
+            return new Rectangle(x, y, w, h);
+        }
+
+        private Rectangle GetImageDisplayRectangle(PictureBox pb, Image img)
+        {
+            if (pb.SizeMode != PictureBoxSizeMode.Zoom)
+                return pb.ClientRectangle;
+
+            float imageRatio = (float)img.Width / (float)img.Height;
+            float boxRatio = (float)pb.ClientSize.Width / (float)pb.ClientSize.Height;
+
+            int drawWidth, drawHeight;
+
+            if (imageRatio > boxRatio)
+            {
+                drawWidth = pb.ClientSize.Width;
+                drawHeight = (int)(pb.ClientSize.Width / imageRatio);
+            }
+            else
+            {
+                drawHeight = pb.ClientSize.Height;
+                drawWidth = (int)(pb.ClientSize.Height * imageRatio);
+            }
+
+            int x = (pb.ClientSize.Width - drawWidth) / 2;
+            int y = (pb.ClientSize.Height - drawHeight) / 2;
+
+            return new Rectangle(x, y, drawWidth, drawHeight);
+        }
+
+        private void FrmImageEditor_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (originalBitmap != null)
+            {
+                originalBitmap.Dispose();
+                originalBitmap = null;
+            }
+            Application.Exit();
+        }
+
+    }
+}
